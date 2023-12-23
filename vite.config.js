d420bf7061276e8154d454d46d0018a3dd6330e4
root <vietkaka9x@gmail.com>
2023-12-23 11:39:30 +0000

    feat: update gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    origin: 'http://127.0.0.1:5175',
     port: 5175
   },
   plugins: [react()]
